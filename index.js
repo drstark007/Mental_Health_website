325d3f759c04f3543db4421bda80c729d1fa1d9a
Pranav Prakhar <56396463+drstark007@users.noreply.github.com>
2021-05-29 10:54:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const express  = require("express")
+const bodyParser = require("body-parser")
+const path = require("path")
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", function(req, res){
+  res.sendFile(path.join(__dirname, "/index.html"))
+})
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000.");
+})
